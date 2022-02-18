f41a143b3c6209f45e4e52b9846f93621a75e240
Sergei Palmer <10038681+s-palmer@users.noreply.github.com>
2022-02-18 15:57:14 +0000

    Initial test cases pass

---
+++ counterReducer/counterReducerExample.js
@@ -0,0 +1,9 @@
+const counter = (state, action) => {
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } else if (action.type === 'DECREMENT' ) {
+    return state - 1;
+  }
+}
+
+module.exports = counter
